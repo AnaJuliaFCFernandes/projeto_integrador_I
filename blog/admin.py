df9663bf15bc0345bf935779e0d34ac040ff3d90
AnaJuliaFernandes <anajuliafcf@gmail.com>
2026-05-01 17:25:19 -0300

    removed 'post' of references

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Post, Cliente, Dispositivo, OrdemServico
+from .models import Cliente, Dispositivo, OrdemServico
 
-admin.site.register(Post)
 admin.site.register(Cliente)
 admin.site.register(Dispositivo)
 admin.site.register(OrdemServico)
