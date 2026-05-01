d9d13ad2ddc0f17efc5f3f33c129e2a3fb98e550
AnaJuliaFernandes <anajuliafcf@gmail.com>
2026-05-01 17:13:54 -0300

    add service orders

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,12 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from .models import OrdemServico
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    ordens = OrdemServico.objects.all().order_by('-data_entrada')
+    return render(request, 'blog/post_list.html', {'ordens': ordens})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
